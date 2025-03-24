2216d8be7cedaa0c0b68d7b833c89d5a3872af37
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-03-24 15:56:35 -0300

    feat: Implemented a Express Server example. chore: added gitignore.

--- restful/index.js
+++ restful/index.js
@@ -1,17 +1,26 @@
 //Aqui, estamos importando o módulo http, que permite criar um servidor web em Node.js.
-const http = require('http');
-/*
-O método http.createServer() cria um servidor HTTP e recebe uma callback function que será chamada toda vez que houver uma requisição.
-Essa função recebe dois parâmetros:
-    req (Request): Contém os dados da requisição, como URL, método (GET, POST, etc.), headers, etc.
-    res (Response): Responsável por enviar uma resposta ao cliente.
-*/
-let server = http.createServer((req,res)=>{
-    console.log('URL:', req.url);
-    console.log('METHOD:', req.method);
-    res.end("Ok");
+const express = require('express');
+
+let app = express();
+
+app.get('/', (req,res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Olá</h1>');
 });
 
-server.listen(3000, '127.0.0.1', ()=>{
+app.get('/users', (req,res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+            users:[{
+                name: "João Victor",
+                idade: "21"
+            }]
+    });
+});
+
+
+app.listen(3000, '127.0.0.1', ()=>{
     console.log("servidor rodando");
 });
